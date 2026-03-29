d78c2ccb43f0b7ff98c7f2514fa0bf70c3d957a7
Montasir Azad <montasirazad@gmail.com>
2026-03-30 02:41:07 +0600

    added new practice folder

--- lws-raw-node-project/practice-4/handlers/tokenHandler.js
+++ lws-raw-node-project/practice-4/handlers/tokenHandler.js
@@ -76,10 +76,71 @@ handler._token.get = (requestProperties, callback) => {
   }
 };
 handler._token.put = (requestProperties, callback) => {
-  callback(200, { msg: "success" });
+  const id =
+    typeof requestProperties.body.id === "string" &&
+    requestProperties.body.id.trim().length === 20
+      ? requestProperties.body.id
+      : false;
+
+  const extend =
+    typeof requestProperties.body.extend === "boolean" &&
+    requestProperties.body.extend === true
+      ? requestProperties.body.extend
+      : false;
+  if (id && extend) {
+    data.read("tokens", id, (err, tokenData) => {
+      let tokenObject = parsedJson(tokenData);
+      if (tokenObject.expires > Date.now()) {
+        tokenObject.expires = Date.now() + 60 * 60 * 1000;
+        data.update("tokens", id, tokenObject, (err) => {
+          if (!err) {
+            callback(200);
+          } else {
+            callback(500, {
+              error: "Server side error.",
+            });
+          }
+        });
+      } else {
+        callback(400, {
+          error: "Token expires.",
+        });
+      }
+    });
+  } else {
+    callback(400, {
+      error: "problem in your request.",
+    });
+  }
 };
 handler._token.delete = (requestProperties, callback) => {
-  callback(200, { msg: "success" });
+  const id =
+    typeof requestProperties.queryStringObject.id === "string" &&
+    requestProperties.queryStringObject.id.trim().length === 20
+      ? requestProperties.queryStringObject.id
+      : false;
+
+  if (id) {
+    data.read("tokens", id, (err, tokenData) => {
+      if (!err && tokenData) {
+        data.delete("tokens", id, (err) => {
+          if (!err) {
+            callback(200, {
+              msg: "Deleted successfully.",
+            });
+          } else {
+            callback(500, { error: "Server error." });
+          }
+        });
+      } else {
+        callback(500, { error: "Server error." });
+      }
+    });
+  } else {
+    callback(400, {
+      error: "Problem in request.",
+    });
+  }
 };
 
 module.exports = handler;
