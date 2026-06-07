8e1d619bf5d1373400994e3afb43c5b1dfbaa72b
Montasir Azad <montasirazad@gmail.com>
2026-06-07 19:26:03 +0600

    intro of middleware and setup

---
+++ express-lws/tutorials/middleware/index.js
@@ -0,0 +1,35 @@
+/** --------------Middleware--------------
+ *
+ * Middleware: Functions that have access to req & res object and next function
+ * Middleware can do
+ * 1. Execute any code.
+ * 2. can change req and res object.
+ * 3. can end request/response cycle.
+ * 4. call next middleware by next()
+ * 5. throw & catch errors.
+ *
+ * Types of middleware =>
+ *   1. application level middleware
+ *   2. router level middleware.
+ *   3. error-handling middleware.
+ *   4. built in middleware.
+ *   5. third-party middleware.
+ */
+
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const myMiddleware = (req, res, next) => {
+  console.log("i am logging.");
+  next();
+};
+
+app.use(myMiddleware);
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
