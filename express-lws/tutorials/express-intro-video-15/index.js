da10bd6df3db106054e472b8b1ff61dde1da6256
Montasir Azad <montasirazad@gmail.com>
2026-06-08 01:48:38 +0600

    practicing express

--- express-lws/tutorials/express-intro-video-15/index.js
+++ express-lws/tutorials/express-intro-video-15/index.js
@@ -1,19 +1,24 @@
 const express = require("express");
 
 const app = express();
-app.use(express.raw());
+app.use(express.json());
 app.use(express.static(__dirname + "/public/",{index:'home.html'}));
 app.get("/", (req, res) => {
   res.send("This is homepage");
 });
 
 app.post("/", (req, res) => {
-  console.log(req.body.toString);
+  console.log(req.body);
   res.send("This is homepage with post request.");
 });
 app.listen(5000, () => {
   console.log(`Listening to port 5000`);
 });
 
-//**used express raw,json,text function
-// */
+// **used express raw,json,text function
+// express.json()
+// express.raw()
+// express.Router()
+// express.static()
+// express.text()
+// express.urlencoded() 
